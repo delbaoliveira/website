470c5444141704f62abfd0ecb71dce8905bc6220
Delba de Oliveira <delba.brown@gmail.com>
2021-05-26 09:32:26 +0100

    Create challenge landing page

---
+++ pages/challenge/index.js
@@ -0,0 +1,11 @@
+import Hero from "@/ui/challenge/Hero"
+import Gallery from "@/ui/challenge/Gallery"
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Gallery />
+    </>
+  )
+}
