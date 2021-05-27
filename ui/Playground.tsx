7d7172611f1eb1376deb4d9fae4a36ed981e998d
Delba de Oliveira <delba.brown@gmail.com>
2021-05-27 11:24:21 +0100

    Add description prop to playground

--- ui/Playground.tsx
+++ ui/Playground.tsx
@@ -1,9 +1,20 @@
 import React from "react"
 
-export function Playground({ children }: { children: React.ReactNode }) {
+export function Playground({
+  children,
+  description,
+}: {
+  children: React.ReactNode
+  description?: string
+}) {
   return (
-    <div className="flex items-center justify-center p-20 mb-4 border border-gray-100 rounded-md polka-bg-gray">
-      {children}
+    <div className="mb-6">
+      <div className="flex items-center justify-center p-20 border border-gray-100 rounded-md polka-bg-gray">
+        {children}
+      </div>
+      {description ? (
+        <div className="mt-3 text-sm text-gray-400">{description}</div>
+      ) : null}
     </div>
   )
 }
