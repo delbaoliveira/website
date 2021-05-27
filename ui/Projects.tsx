94cdd2f6a91ccafeef7e51eb9463dea378f0ca5d
Delba de Oliveira <delba.brown@gmail.com>
2021-05-27 16:57:05 +0100

    Clear unused code

--- ui/Projects.tsx
+++ ui/Projects.tsx
@@ -1,7 +1,7 @@
 import { darkColors } from "@/ui/brandColors"
 import { FOCUS_VISIBLE_OUTLINE } from "@/ui/constants"
 import { useHover } from "@react-aria/interactions"
-import clsx from "clsx"
+import cx from "clsx"
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
@@ -16,47 +16,12 @@ type Project = {
   url?: string
 }
 
-const data: Project[] = [
-  {
-    name: "30 day front-end challenge",
-    description:
-      "30 days of front-end is a series of daily challenges recreating simple yet delightful UI elements from around the web.",
-    url: "/challenge",
-    image: "/challenge.png",
-  },
-  {
-    name: "Next.js & MDX blog",
-    description:
-      "A blog using MDX which allows us to import and embed components inside markdown files.",
-    url: "/blog/mdx-nextjs-blog",
-    image: "/blog.png",
-  },
-  // {
-  //   name: "Dough",
-  //   description:
-  //     "A personal budgeting app that makes it easier to manage my finances in one place by pulling data from my UK bank accounts using TrueLayer.",
-  //   image: "/dough.png",
-  // },
-  // {
-  //   name: "Contra",
-  //   description:
-  //     "An iOS app that allows users to create custom pomodoro timers and sessions. It connects to Google Calendar so users can see an overview of their day.",
-  //   image: "/contra.jpeg",
-  // },
-  // {
-  //   name: "Bella Pilates",
-  //   description:
-  //     "A pilates platform offering beginner to advanced programs and nutritional plans for Portuguese-speaking audiences.",
-  //   image: "/bella-julia.png",
-  // },
-]
-
 const Project = ({ project, color }: { project: Project; color: string }) => {
   let { hoverProps, isHovered } = useHover({})
 
   return (
     <Link href={project.url ? project.url : "/"}>
-      <a className={clsx("block rounded-xl", FOCUS_VISIBLE_OUTLINE)}>
+      <a className={cx("block rounded-xl", FOCUS_VISIBLE_OUTLINE)}>
         <div {...hoverProps}>
           {project.image ? (
             <Tilt
@@ -107,8 +72,8 @@ export const Projects = ({ projects }: { projects: PostMeta[] }) => {
   return (
     <div className="container px-4 mx-auto">
       <h2 className="text-3xl font-bold text-gray-800">Projects</h2>
-      <h4 className="mt-2 text-gray-700 lg:text-lg">
-        Some of the side projects I'm currently working on:
+      <h4 className="mt-2 text-gray-500 lg:text-lg">
+        Some of the side projects I'm currently working on
       </h4>
 
       <div className="-mt-2 lg:flex lg:flex-wrap lg:-mx-6">
@@ -127,14 +92,6 @@ export const Projects = ({ projects }: { projects: PostMeta[] }) => {
             </div>
           )
         })}
-
-        {/* {data.map((project, index) => {
-          return (
-            <div key={index} className="mt-12 lg:w-1/2 lg:px-6">
-              <Project project={project} color={darkColors[index]} />
-            </div>
-          )
-        })} */}
       </div>
     </div>
   )
