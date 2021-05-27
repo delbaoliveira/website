65860a4f586340fdaf02a0206c78dcdbaeef98b1
Delba de Oliveira <delba.brown@gmail.com>
2021-05-27 16:52:46 +0100

    Redirect old posts

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/challenge",
+        destination: "/#projects",
+        permanent: true,
+      },
+      {
+        source: "/challenge/about",
+        destination: "/blog/challenge-00",
+        permanent: true,
+      },
+      {
+        source: "/challenge/day-01",
+        destination: "/blog/challenge-01",
+        permanent: true,
+      },
+    ]
+  },
+}
