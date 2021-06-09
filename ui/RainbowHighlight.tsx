7ee8d5823dfa3cff9c8bb66cb6ed7df7f0ec55b0
Delba de Oliveira <delba.brown@gmail.com>
2021-06-09 11:12:58 +0100

    Add default prop to RainbowHighlight

--- ui/RainbowHighlight.tsx
+++ ui/RainbowHighlight.tsx
@@ -4,16 +4,18 @@ import { RoughNotation } from "react-rough-notation"
 export const RainbowHighlight = ({
   color,
   children,
+  type = "highlight",
 }: {
   color: string
   children: string
+  type: string
 }) => {
   // Change the animation duration depending on length of text we're animating (speed = distance / time)
   const animationDuration = Math.floor(30 * children.length)
 
   return (
     <RoughNotation
-      type="highlight"
+      type={type}
       multiline={true}
       padding={[0, 2]}
       iterations={1}
