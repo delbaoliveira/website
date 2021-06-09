34ac2d4c98914507d2d2d0c61467693fe6e767ca
Delba de Oliveira <delba.brown@gmail.com>
2021-06-09 10:57:06 +0100

    Update link and roughnotation colors

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,16 +2,7 @@
 // using the keyboard)
 export const FOCUS_VISIBLE_OUTLINE = `focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-lightBlue-500 focus-visible:ring-opacity-50 focus-visible:outline-none focus:outline-none`
 
-export const GRADIENT_LINK = `decoration-clone bg-clip-text font-medium text-transparent bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 hover:text-pink-600 hover:bg-none`
+export const GRADIENT_LINK = `decoration-clone bg-clip-text text-transparent bg-gradient-to-l  from-blue-500 to-blue-700
+hover:text-green-700 hover:bg-none`
 
-export const LIGHT_COLORS = ["#E9D5FF", "#FBCFE8", "#FECACA", "#FDE68A"]
-
-export const DARK_COLORS = [
-  "#FB7185",
-  "#FBBF24",
-  "#34D399",
-  "#E879F9",
-  "#38BDF8",
-  "#9CA3AF",
-  "#FB923C",
-]
+export const LIGHT_COLORS = ["#FDE68A", "#BBF7D0", "#BFDBFE"]
