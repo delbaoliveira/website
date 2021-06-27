49755d7537027c9d5f2bb0cc674cea2335f061d4
Delba de Oliveira <delba.brown@gmail.com>
2021-06-27 22:20:59 +0100

    Remove job op wording from Contact

--- ui/Contact.tsx
+++ ui/Contact.tsx
@@ -1,38 +1,17 @@
 import { GRADIENT_LINK } from "@/lib/constants"
+import { RoughNotation } from "react-rough-notation"
 
 export const Contact = () => {
   return (
     <div className="container px-4 mx-auto">
-      <h2 className="text-3xl font-bold text-gray-800">Get in touch</h2>
-      <p className="mt-2 text-gray-700 lg:text-lg">
-        Do you have a job opportunity or idea you'd like to discuss? Feel free
-        to reach me at{" "}
-        <a href="mailto:hello@delbaoliveira.com" className={GRADIENT_LINK}>
+      <h2 className="text-2xl font-bold text-gray-800">Get in touch</h2>
+      <p className="mt-6 text-gray-800">
+        Do you have an idea you'd like to discuss? Feel free to reach me at{" "}
+        <a
+          href="mailto:hello@delbaoliveira.com"
+          className="font-medium transition-colors text-emerald-500 hover:text-sky-500"
+        >
           hello@delbaoliveira.com
-        </a>
-        . You can also find me on{" "}
-        <a
-          href="https://twitter.com/delba_oliveira"
-          target="_blank"
-          className={GRADIENT_LINK}
-        >
-          Twitter
-        </a>
-        ,{" "}
-        <a
-          href="https://github.com/delbaoliveira"
-          target="_blank"
-          className={GRADIENT_LINK}
-        >
-          Github
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://www.linkedin.com/in/delbaoliveira/"
-          target="_blank"
-          className={GRADIENT_LINK}
-        >
-          Linkedin
         </a>
         .
       </p>
