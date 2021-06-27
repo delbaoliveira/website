bd34a080392c2cc2a3c861e7f1607569ce899301
Delba de Oliveira <delba.brown@gmail.com>
2021-06-27 22:20:42 +0100

    Add social links to footer

--- ui/Footer.tsx
+++ ui/Footer.tsx
@@ -1,18 +1,53 @@
+import { RoughNotation } from "react-rough-notation"
+import cx from "clsx"
+import { FOCUS_VISIBLE_OUTLINE } from "@/lib/constants"
+
 export const Footer = () => {
   return (
-    <div className="container px-4 mx-auto mt-24 mb-96">
-      <div className="mb-8 border-t border-gray-200"></div>
-      <div className="text-gray-700">
-        <p>
-          Built with TypeScript, Tailwind and Next.js{" "}
-          <a
-            className="hover:text-lightBlue-400"
-            title="Much love <3"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          >
-            ♥
-          </a>
-        </p>
+    <div className="mt-24 pb-36">
+      <div className="max-w-4xl px-4 mx-auto text-gray-700">
+        <div className="pb-8 mb-2 border-t-2 border-gray-300"></div>
+        <div className="flex justify-between ">
+          <p>Built with Tailwind and Next.js 🖤</p>
+          <div className="space-x-6 font-medium">
+            <a
+              href="https://twitter.com/delba_oliveira"
+              className={cx(
+                "transition-colors hover:text-sky-500 focus:text-emerald-500 rounded",
+                FOCUS_VISIBLE_OUTLINE,
+              )}
+            >
+              Twitter
+            </a>
+            <a
+              href="https://github.com/delbaoliveira"
+              className={cx(
+                "transition-colors hover:text-sky-500 focus:text-emerald-500 rounded",
+                FOCUS_VISIBLE_OUTLINE,
+              )}
+            >
+              Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/delbaoliveira/"
+              className={cx(
+                "transition-colors hover:text-sky-500 focus:text-emerald-500 rounded",
+                FOCUS_VISIBLE_OUTLINE,
+              )}
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://www.instagram.com/delba.oliveira/"
+              className={cx(
+                "transition-colors hover:text-sky-500 focus:text-emerald-500 rounded",
+                FOCUS_VISIBLE_OUTLINE,
+              )}
+            >
+              Instagram
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
