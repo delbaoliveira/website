a8b2b8f6c36d850e73ff7d5195bde510f30ff434
Delba de Oliveira <delba.brown@gmail.com>
2021-06-27 22:06:47 +0100

    Delete unused dark colors and update light colors

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,14 +4,4 @@ export const FOCUS_VISIBLE_OUTLINE = `focus-visible:ring-2 focus-visible:ring-of
 
 export const GRADIENT_LINK = `decoration-clone bg-clip-text font-medium text-transparent bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 hover:text-pink-600 hover:bg-none`
 
-export const LIGHT_COLORS = ["#E9D5FF", "#FBCFE8", "#FECACA", "#FDE68A"]
-
-export const DARK_COLORS = [
-  "#FB7185",
-  "#FBBF24",
-  "#34D399",
-  "#E879F9",
-  "#38BDF8",
-  "#9CA3AF",
-  "#FB923C",
-]
+export const LIGHT_COLORS = ["#A7F3D0", "#BAE6FD", "#C7D2FE", "#FDE68A"]
