090f09a8521a9f91daa4cd03be20c86c7e7602ef
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-07-04 22:12:55 +0100

    Update BlogPreview.tsx

--- ui/BlogPreview.tsx
+++ ui/BlogPreview.tsx
@@ -6,17 +6,14 @@ import type { PostMeta } from "types/post"
 export const BlogPreview = ({ post }: { post: PostMeta }) => {
   return (
     <div key={post.slug}>
-      <h2 className="text-lg font-bold">
-        <Link href={`/blog/${post.slug}`}>
-          <a className="text-gray-800 transition-colors hover:text-sky-500">
+      <Link href={`/blog/${post.slug}`}>
+        <a className="text-gray-800 group">
+          <h2 className="text-lg font-bold group-hover:text-sky-500">
             {post.title}
-          </a>
-        </Link>
-      </h2>
-      <div className="text-sm text-gray-500">
-        {format(parseISO(post.publishedAt), "MMMM dd, yyyy")}
-      </div>
-      <div className="mt-2 text-gray-700 line-clamp-2">{post.description}</div>
+          </h2>
+          <p className="text-gray-800 line-clamp-2">{post.description}</p>
+        </a>
+      </Link>
     </div>
   )
 }
