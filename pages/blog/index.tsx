efc34bcf5eadcc29c48a912c5345d2728b8889ce
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-07-05 09:43:38 +0100

    Make blog index style consistent with home page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -12,12 +12,12 @@ export function getStaticProps() {
 export default function BlogPage({ posts }: { posts: PostMeta[] }) {
   return (
     <Layout>
-      <div className="container max-w-3xl px-4 mx-auto mt-24">
+      <div className="container px-4 mx-auto mt-24">
         <h1 className="text-4xl font-extrabold text-gray-800">Blog</h1>
-        <h4 className="mt-2 text-gray-700 lg:text-lg">
+        <h4 className="mt-2 text-gray-500">
           Thoughts on what I'm building and learning.
         </h4>
-        <div className="mt-12 space-y-12">
+        <div className="mt-8 space-y-8">
           {posts.map((post) => (
             <BlogPreview key={post.slug} post={post} />
           ))}
