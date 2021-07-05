9b2482de54d89dd3df21373194a46aae5be145db
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-07-05 09:43:51 +0100

    Update footer text color

--- ui/Footer.tsx
+++ ui/Footer.tsx
@@ -5,7 +5,7 @@ import { FOCUS_VISIBLE_OUTLINE } from "@/lib/constants"
 export const Footer = () => {
   return (
     <div className="mt-24 pb-36">
-      <div className="max-w-4xl px-4 mx-auto text-gray-700">
+      <div className="max-w-4xl px-4 mx-auto text-gray-800">
         <div className="pb-8 mb-2 border-t-2 border-gray-300"></div>
         <div className="flex flex-col justify-between lg:flex-row">
           <p>Built with Tailwind and Next.js 🖤</p>
