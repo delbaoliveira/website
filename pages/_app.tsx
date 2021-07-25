1da8fd2274cd11d6d312192b68c584d8f1380e0e
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-07-25 16:15:45 +0100

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
+import { useAnalytics } from "@/lib/useAnalytics"
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
+  useAnalytics()
+
   return (
     <>
       <Head>
