10744414beca9b5dc423ec005adf0f4fd56f6869
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-07-26 12:04:10 +0100

    Test plausible analytics

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/iguana/js/script.js",
+        destination: "https://plausible.io/js/plausible.js",
+      },
+      {
+        source: "/iguana/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ]
+  },
   async redirects() {
     return [
       {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { useAnalytics } from "@/lib/useAnalytics"
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
 import Head from "next/head"
+import Script from "next/script"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Script
+        src="/iguana/js/script.js"
+        data-domain="delbaoliveira.com"
+        data-api="/iguana/api/event"
+      />
       <Head>
         <title>Delba de Oliveira</title>
       </Head>
