a65c88d1d190cb3e14fe2405dfc9042a69b53d06
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-08-01 00:07:14 +0100

    Add new video

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,12 @@ export const getStaticProps = async () => {
 
 const videos = [
   {
+    title: "My Story & How I Became a Developer without a Degree",
+    image: "/blog/nqLNe92yIqw.png",
+    url: "https://www.youtube.com/watch?v=nqLNe92yIqw",
+    text: "The story of how I dropped out of medical school, left home at 18, and eventually learned how to code. It spans 3 countries and a decade, it's been a long journey with a few detours, but I eventually found my career path.",
+  },
+  {
     title:
       "How I Built the Portfolio That Landed Me a Dev Role (Tech Stack Explained)",
     image: "/blog/zl9iXZrw_dw.png",
@@ -31,39 +37,40 @@ export default function Home({
           <About />
         </div>
 
-        <div id="blog">
-          <div className="container px-4 mx-auto">
-            <h2 className="text-2xl font-bold text-gray-800">Recent Posts</h2>
-            <h4 className="mt-2 text-gray-500">
-              Thoughts on what I'm building and learning.
-            </h4>
-            {posts.map((post) => (
-              <div
-                key={post.slug}
-                className="mt-6 sm:grid sm:grid-cols-2 sm:gap-10"
-              >
-                <PostPreview post={post} />
-              </div>
-            ))}
-          </div>
-        </div>
-
         <div id="reel">
           <div className="container px-4 mx-auto">
             <h2 className="text-2xl font-bold text-gray-800">Recent Videos</h2>
             <h4 className="mt-2 text-gray-500">
               Videos on what I'm building and learning.
             </h4>
-            <div className="mt-6 sm:grid sm:grid-cols-2 sm:gap-10">
+            <div className="sm:grid sm:grid-cols-2 sm:gap-10">
               {videos.map((video) => (
-                <MediaPreview
-                  key={video.url}
-                  type="youtube"
-                  url={video.url}
-                  title={video.title}
-                  text={video.text}
-                  image={video.image}
-                />
+                <div key={video.url} className="mt-10">
+                  <MediaPreview
+                    type="youtube"
+                    url={video.url}
+                    title={video.title}
+                    text={video.text}
+                    image={video.image}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div id="blog">
+          <div className="container px-4 mx-auto">
+            <h2 className="text-2xl font-bold text-gray-800">Recent Posts</h2>
+            <h4 className="mt-2 text-gray-500">
+              Thoughts on what I'm building and learning.
+            </h4>
+
+            <div className="sm:grid sm:grid-cols-2 sm:gap-10">
+              {posts.map((post) => (
+                <div key={post.slug} className="mt-10">
+                  <PostPreview post={post} />
+                </div>
               ))}
             </div>
           </div>
