09740db5cd1c6b99dc7dee862ab66a27c8e42088
Delba de Oliveira <delba.brown@gmail.com>
2022-01-24 23:08:58 +0000

    Update og.ts

--- lib/og.ts
+++ lib/og.ts
@@ -10,7 +10,7 @@ export const createOgImage = ({
 }) => {
   return `https://res.cloudinary.com/delba/image/upload/c_scale,w_1600,h_836,q_95/c_fit,co_rgb:FFF,l_text:Karla_64_bold:${e(
     t,
-  )},w_1400/fl_layer_apply,g_south_west,x_100,y_160/c_fit,co_rgb:FFFFFF70,l_text:Karla_40:${e(
+  )},w_1400/fl_layer_apply,g_south_west,x_100,y_160/c_fit,co_rgb:FFFFFF75,l_text:Karla_40:${e(
     m,
   )},w_1400/fl_layer_apply,g_south_west,x_100,y_100/l_delba/fl_layer_apply,w_140,g_north_west,x_100,y_100/bg.png`
 }
