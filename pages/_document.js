da721b4a2c7d96ca0531a8d8c569fe2ec9876ce3
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2022-01-30 16:11:10 +0000

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,23 +10,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
+          <link rel="icon" type="image/png" href="/assets/favicon.png?v=1" />
           <meta name="theme-color" content="#0e0c0b" />
         </Head>
 
