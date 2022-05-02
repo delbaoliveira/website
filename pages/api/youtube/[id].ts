97bed251415f8237d9b0161279e87cb2f5ae2eb7
Delba de Oliveira <delba.brown@gmail.com>
2022-05-02 09:57:44 +0100

    Simplify youtube API

--- pages/api/youtube/[id].ts
+++ pages/api/youtube/[id].ts
@@ -9,7 +9,7 @@ export default async function handler(
     const id = z.string().parse(req.query.id)
 
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=contentDetails%2Csnippet%2Cstatistics&id=${id}&key=${process.env.YOUTUBE_DATA_API_KEY}`,
+      `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${id}&key=${process.env.YOUTUBE_DATA_API_KEY}`,
     ).then((x) => x.json())
 
     if (!response.items?.[0]) {
@@ -18,12 +18,8 @@ export default async function handler(
 
     const video = response.items[0]
 
-    const duration = video.contentDetails?.duration
     const views = video.statistics?.viewCount
     const likes = video.statistics?.likeCount
-    const thumbnail = video.snippet?.thumbnails?.maxres?.url
-    const title = video.snippet?.title
-    const publishedAt = video.snippet?.publishedAt
 
     res.setHeader(
       "Cache-Control",
@@ -33,26 +29,6 @@ export default async function handler(
     return res.status(200).json({
       views: views ? Number(views) : 0,
       likes: likes ? Number(likes) : 0,
-      duration: duration
-        ? duration
-            // PT1M2S => 01:02
-            .replace(/PT/, "")
-            .replace(/H/, ":")
-            .replace(/M/, ":")
-            .replace(/S/, "")
-            .split(":")
-            .map((digit: string) => {
-              if (Number(digit) <= 9) {
-                return "0" + digit
-              } else {
-                return digit
-              }
-            })
-            .join(":")
-        : 0,
-      thumbnail,
-      title,
-      publishedAt,
     })
   } catch (err: any) {
     console.error(err.message)
