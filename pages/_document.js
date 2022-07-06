3d51a08427203600609a21e51b7cfb4ad426b96d
Delba de Oliveira <delbabrown@gmail.com>
2022-07-06 13:34:31 +0100

    Move Document to Function Component

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import { Html, Head, Main, NextScript } from "next/document"
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en" className="scroll-p-24">
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="icon" type="image/png" href="/assets/favicon.png?v=1" />
-          <meta name="theme-color" content="#0e0c0b" />
-        </Head>
+export default function Document() {
+  return (
+    <Html lang="en" className="scroll-p-24">
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" type="image/png" href="/assets/favicon.png?v=1" />
+        <meta name="theme-color" content="#0e0c0b" />
+      </Head>
 
-        <body className="bg-[#0e0c0b]">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+      <body className="bg-[#0e0c0b]">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
