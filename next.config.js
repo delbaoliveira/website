ec54f2a4779990d66f5ee4a9c067c7b4129bc8cd
Delba de Oliveira <delbabrown@gmail.com>
2022-07-06 13:59:21 +0100

    Start using next future image

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const { withContentlayer } = require("next-contentlayer")
  * @type {import('next').NextConfig}
  */
 module.exports = withContentlayer({
+  experimental: {
+    images: {
+      allowFutureImage: true,
+      // allow next/image to serve remote images from safelisted domains
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+          pathname: "/delba/**",
+        },
+        { hostname: "api.microlink.io" },
+        { hostname: "pbs.twimg.com" },
+      ],
+    },
+  },
   async redirects() {
     return [
       {
@@ -17,11 +32,5 @@ module.exports = withContentlayer({
         permanent: true,
       },
     ]
-  },
-  images: {
-    domains: [
-      "api.microlink.io", // Link previews
-      "pbs.twimg.com", // Twitter Profile Picture
-    ],
   },
 })
