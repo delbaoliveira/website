9d76c4c3a882c941d25cc1c50b7b76b25d97e746
Delba de Oliveira <delbabrown@gmail.com>
2022-07-06 13:59:40 +0100

    Add border to images

--- ui/BlurImage.tsx
+++ ui/BlurImage.tsx
@@ -9,16 +9,16 @@ export const BlurImage = (props: ImageProps) => {
   return (
     <div
       className={cx(
-        "flex overflow-hidden rounded-xl bg-white/[2%]",
+        "relative flex overflow-hidden rounded-xl bg-white/[2%] after:pointer-events-none after:absolute after:inset-0 after:z-10 after:rounded-xl after:border after:border-rose-200/10 after:content-['']",
         isLoading ? "animate-pulse" : "",
       )}
     >
       <NextImage
         {...props}
         className={cx(
-          "duration-700 ease-in-out",
+          "rounded-xl duration-700 ease-in-out",
           isLoading
-            ? "scale-105 blur-xl grayscale"
+            ? "scale-[1.02] blur-xl grayscale"
             : "scale-100 blur-0 grayscale-0",
         )}
         onLoadingComplete={() => setLoading(false)}
