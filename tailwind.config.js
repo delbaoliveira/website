89d906a44a56d27f0b0f7cab1cb20ba58b9e82cd
Delba de Oliveira <delba.brown@gmail.com>
2022-07-11 19:30:59 +0100

    fix animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
       keyframes: ({ theme }) => ({
         mutation: {
           "0%": {
-            background: theme("colors.rose.200"),
+            background: theme("colors.rose.200 / 3%"),
           },
           "10%": {
             background: theme("colors.rose.200 / 15%"),
