7be489e1bd4ad960a0cb37b60b2173865da41ffe
Delba de Oliveira <delba.brown@gmail.com>
2022-07-18 23:01:41 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
-const plugin = require("tailwindcss/plugin")
 const colors = require("tailwindcss/colors")
 
 /** @type {import('tailwindcss').Config} */
