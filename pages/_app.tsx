aa527027b85686c2105120968d2ccae9c4f7431b
Delba de Oliveira <delbabrown@gmail.com>
2022-11-20 15:32:43 +0000

    Remove unused analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import { seo } from "@/lib/seo"
-import { useAnalytics } from "@/lib/useAnalytics"
 import { DefaultSeo } from "next-seo"
 import type { AppProps } from "next/app"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  useAnalytics()
-
   return (
     <>
       <DefaultSeo {...seo} />
