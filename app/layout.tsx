65db8d71174a00aa294de695205292657ca70926
Delba de Oliveira <delbabrown@gmail.com>
2023-11-16 09:59:37 +0000

    Add styles and metadata constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,39 @@
 import localFont from "next/font/local"
 import cx from "clsx"
 import "@/styles/globals.css"
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
+import { createOgImage } from "@/lib/createOgImage"
+import { PreloadResources } from "@/lib/preload"
+
+const title = `Delba`
+const description = `Welcome to my digital garden where I share what I'm learning about shipping great products, becoming a better developer and growing a career in tech.`
+const domain = `delba.dev`
+const twitterHandle = `@delba_oliveira`
+const meta = `Developer Experience at ▲ Vercel and Next.js`
+
+export const viewport: Viewport = {
+  themeColor: "#1c1917",
+}
 
 export const metadata: Metadata = {
-  title: { template: "%s | Delba", default: "Delba" },
-  description:
-    "Welcome to my digital garden where I share what I'm learning about shipping great products, becoming a better developer and growing a career in tech.",
+  title: { template: `%s | ${title}`, default: title },
+  description: description,
   twitter: {
-    creator: "@delba_oliveira",
+    creator: twitterHandle,
   },
   openGraph: {
     title: "Delba",
-    url: `https://delba.dev`,
+    url: `https://${domain}/`,
     siteName: "Delba",
     // TODO: Migrate OG image to use ImageResponse
-    // images: [
-    //   {
-    //     url: createOgImage({ title, meta }),
-    //     width: 1600,
-    //     height: 836,
-    //     alt: "Delba",
-    //   },
-    // ],
+    images: [
+      {
+        url: createOgImage({ title, meta }),
+        width: 1600,
+        height: 836,
+        alt: "Delba",
+      },
+    ],
   },
 }
 
@@ -39,8 +50,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={cx("antialiased font-sans", hubot.variable)}>
+    <html lang="en" className="scroll-p-24">
+      <body
+        className={cx(
+          "antialiased font-sans bg-gray-900 selection:bg-purple-500/90 selection:text-white",
+          hubot.variable,
+        )}
+      >
+        <PreloadResources />
         {children}
       </body>
     </html>
