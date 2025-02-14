96f67df7f3d1bdbed2fefbefa4618bc30c6e0d07
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2025-02-14 11:20:47 +0000

    Update tailwind (#61)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
   },
-};
+}
 
-export default config;
+export default config
